18456fd0de03e16e021b1be3e7e5617c1a0bf3e9
unknown <danielmunz82@gmail.com>
2021-03-20 20:11:00 -0500

    Segundo commit

---
+++ Prueba_1/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Prueba_1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Prueba Técnica.&nbsp;&nbsp; ASP.NET</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <asp:Button ID="BtnListarUsuarios" runat="server" Text="Listar usuarios" OnClick="Button1_Click" CssClass="info" />
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <asp:GridView ID="GridView1" runat="server" AllowPaging="True" CellPadding="10" ForeColor="#333333" GridLines="None" OnPageIndexChanging="GridView1_PageIndexChanging" PageSize="20" CellSpacing="8" Height="100%" HorizontalAlign="Left" Width="100%">
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#CCCCCC" />
+                <FooterStyle BackColor="#999999" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#999999" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#999999" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#EFF3FB" BorderStyle="None" />
+                <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+                
+            </asp:GridView>
+        </div>
+    </div> 
+    <div class="row">
+        <div class="col-md-12">
+            <asp:TextBox ID="TextBox1" runat="server" BorderStyle="None" Enabled="False" ReadOnly="True" Width="100%" Wrap="False" Font-Bold="True" ForeColor="#FF6666"></asp:TextBox>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Prueba_1/Default.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Reflection;
+
+namespace Prueba_1
+{
+    public partial class _Default : Page
+    {
+        //Declaracion de variables y objetos
+        DataTable dtable = new DataTable();
+        DataColumn dc;
+        DataRow dr;
+
+        LanderServices.User_WebServiceSoapClient usuariosRequest = new LanderServices.User_WebServiceSoapClient();
+        LanderServices.UsuariosAplic[] usuarios;
+        PropertyInfo[] Propiedades;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            dtable = ConsultarServicio();
+            if (dtable != null)
+            {
+                GridView1.DataSource = dtable;
+                GridView1.DataBind();
+            }
+            else
+            {
+                TextBox1.Text = "No hay información que mostrar.";
+            }
+        }
+
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            dtable = ConsultarServicio();
+            if (dtable != null)
+            {
+                GridView1.DataSource = dtable;
+                GridView1.PageIndex = e.NewPageIndex;
+                GridView1.DataBind();
+            }
+            else
+            {
+                TextBox1.Text = "No hay información que mostrar.";
+            }
+        }
+
+        private DataTable ConsultarServicio()
+        {
+            //Identificacion dinamica de columnas
+            Propiedades = typeof(LanderServices.UsuariosAplic).GetProperties();
+            foreach (PropertyInfo Propiedad in Propiedades)
+            {
+                if (Propiedad.Name != "ExtensionData" && Propiedad.Name != "param" && Propiedad.Name != "Codigo")
+                {
+                    dc = new DataColumn(Propiedad.Name, Propiedad.PropertyType);
+                    dtable.Columns.Add(dc);
+                }
+            }
+
+            //Consumo de Servicio ListarUsuarios
+            try
+            {
+                usuarios = usuariosRequest.ListarUsuarios("Prueba1", "Prueba1");
+                foreach (LanderServices.UsuariosAplic usuario in usuarios)
+                {
+                    dr = dtable.NewRow();
+                    foreach (PropertyInfo Propiedad in Propiedades)
+                    {
+                        if (Propiedad.Name != "ExtensionData" && Propiedad.Name != "param" && Propiedad.Name != "Codigo")
+                        {
+                            dr[Propiedad.Name] = usuario.GetType().GetProperty(Propiedad.Name).GetValue(usuario);
+                        }
+                    }
+                    dtable.Rows.Add(dr);
+                }
+                return dtable;
+            }
+            catch (Exception ex)
+            {
+                TextBox1.Text = "Ocurrió un error al acceder al servicio. Consulte al administrador.";
+                return null;
+            }
+        }
+    }
+}
